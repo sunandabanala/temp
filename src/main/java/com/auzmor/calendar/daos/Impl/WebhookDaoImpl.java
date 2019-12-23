48694fd4db210e87c6a2893cde09fa65157fada6
geetaa_sravanthii <geeta@auzmor.com>
2019-12-23 15:11:46 +0530

    null check fix

--- src/main/java/com/auzmor/calendar/daos/Impl/WebhookDaoImpl.java
+++ src/main/java/com/auzmor/calendar/daos/Impl/WebhookDaoImpl.java
@@ -62,7 +62,8 @@ public class WebhookDaoImpl implements WebhookDao {
             int isDeleted = e2.getStatus().equals("cancelled") ? 1 : 0;
             updateEvents = getEventsToUpdate(calendarEventId, gson.toJson(e2), updateEvents, isDeleted);
             platformUpdateEvents = getPlatformEventsToUpdate(objectDetailsMap.get(calendarEventId).getEventId(), e2, objectDetailsMap.get(calendarEventId).getTimeZone(), platformUpdateEvents);
-            if (c != null && c.getWhen().getEnd_time() != e2.getWhen().getEnd_time() || c.getWhen().getStart_time() != e2.getWhen().getStart_time() || !c.getLocation().equals(e2.getLocation()) || e2.getStatus().equals("cancelled")) {
+            Boolean locationUpdated = (e2.getLocation() != null && c.getLocation() == null) || (e2.getLocation() != null && c.getLocation() != null && !(c.getLocation().equals(e2.getLocation())));
+            if (c != null && c.getWhen().getEnd_time() != e2.getWhen().getEnd_time() || c.getWhen().getStart_time() != e2.getWhen().getStart_time() || locationUpdated || e2.getStatus().equals("cancelled")) {
               String secondObjectId = eventObjectMap.get(objectDetailsMap.get(calendarEventId).getEventId()).get(0).equals(calendarEventId) ? eventObjectMap.get(objectDetailsMap.get(calendarEventId).getEventId()).get(1) : eventObjectMap.get(objectDetailsMap.get(calendarEventId).getEventId()).get(0) ;
               CalendarEvent c2 = gson.fromJson(objectDetailsMap.get(secondObjectId).getCalendarDetails(), CalendarEvent.class);
               c2.getWhen().setEnd_time(e2.getWhen().getEnd_time());
