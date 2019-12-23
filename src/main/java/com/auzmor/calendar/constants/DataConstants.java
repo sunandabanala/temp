530edb12a1d225afdc29ff115fee25831ed56d87
Pooja Gautam <pooja@auzmor.com>
2019-12-23 07:41:30 +0000

    Merged in feature/abc (pull request #35)
    Feature/abc
    
    * gettoken by userid
    
    * gettoken by userid
    
    * changes

--- src/main/java/com/auzmor/calendar/constants/DataConstants.java
+++ src/main/java/com/auzmor/calendar/constants/DataConstants.java
@@ -3,7 +3,7 @@ package com.auzmor.calendar.constants;
 import java.util.UUID;
 
 public class DataConstants {
-  public static final String DUMMY_EMAIL =  "75e5ab6f34954c27985dec2f70f7875c"+"@auzmor.com";
+  public static final String DUMMY_EMAIL = UUID.randomUUID().toString().replace("-", "")+"@auzmor.com";
   public static final String PLATFORM_HOST = "platform_host";
   public static final String DEFAULT_MAIL="auzmor-hire-events@auzmor.com";
 
