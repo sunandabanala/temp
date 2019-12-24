485a5082d4a6728007eb63b1f97acf390c22bda4
geetaa_sravanthii <geeta@auzmor.com>
2019-12-24 16:43:17 +0530

    handling multiple accounts;

--- src/main/java/com/auzmor/calendar/daos/AccountDao.java
+++ src/main/java/com/auzmor/calendar/daos/AccountDao.java
@@ -3,9 +3,11 @@ package com.auzmor.calendar.daos;
 import com.auzmor.calendar.models.UserAccount;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Component
 public interface AccountDao {
-  UserAccount getAccount(String nylasAccountId);
+  List<UserAccount> getAccount(String nylasAccountId);
   void updateAccount(String nylasAccountId, String cursorId);
 
   void  addNylasAccount(final UserAccount userAccount);
